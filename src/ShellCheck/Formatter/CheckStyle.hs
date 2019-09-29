961b9c9cb32212a7b97cb65bb1c460a86158ce75
Shane Kennedy <shane.kennedy19@gmail.com>
2019-09-29 17:11:23 +0200

    Reformat CheckStyle

--- src/ShellCheck/Formatter/CheckStyle.hs
+++ src/ShellCheck/Formatter/CheckStyle.hs
@@ -17,10 +17,12 @@
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <https://www.gnu.org/licenses/>.
 -}
-module ShellCheck.Formatter.CheckStyle (format) where
+module ShellCheck.Formatter.CheckStyle
+  ( format
+  ) where
 
-import ShellCheck.Interface
 import ShellCheck.Formatter.Format
+import ShellCheck.Interface
 
 import Data.Char
 import Data.List
@@ -28,66 +30,79 @@ import GHC.Exts
 import System.IO
 
 format :: IO Formatter
-format = return Formatter {
-    header = do
-        putStrLn "<?xml version='1.0' encoding='UTF-8'?>"
-        putStrLn "<checkstyle version='4.3'>",
-
-    onFailure = outputError,
-    onResult = outputResults,
-
-    footer = putStrLn "</checkstyle>"
-}
+format =
+  return
+    Formatter
+      { header =
+          do putStrLn "<?xml version='1.0' encoding='UTF-8'?>"
+             putStrLn "<checkstyle version='4.3'>"
+      , onFailure = outputError
+      , onResult = outputResults
+      , footer = putStrLn "</checkstyle>"
+      }
 
 outputResults cr sys =
-    if null comments
+  if null comments
     then outputFile (crFilename cr) "" []
     else mapM_ outputGroup fileGroups
   where
     comments = crComments cr
     fileGroups = groupWith sourceFile comments
     outputGroup group = do
-        let filename = sourceFile (head group)
-        result <- (siReadFile sys) filename
-        let contents = either (const "") id result
-        outputFile filename contents group
+      let filename = sourceFile (head group)
+      result <- (siReadFile sys) filename
+      let contents = either (const "") id result
+      outputFile filename contents group
 
 outputFile filename contents warnings = do
-    let comments = makeNonVirtual warnings contents
-    putStrLn . formatFile filename $ comments
+  let comments = makeNonVirtual warnings contents
+  putStrLn . formatFile filename $ comments
 
-formatFile name comments = concat [
-    "<file ", attr "name" name, ">\n",
-        concatMap formatComment comments,
-    "</file>"
+formatFile name comments =
+  concat
+    [ "<file "
+    , attr "name" name
+    , ">\n"
+    , concatMap formatComment comments
+    , "</file>"
     ]
 
-formatComment c = concat [
-    "<error ",
-    attr "line" $ show . lineNo $ c,
-    attr "column" $ show . colNo $ c,
-    attr "severity" . severity $ severityText c,
-    attr "message" $ messageText c,
-    attr "source" $ "ShellCheck.SC" ++ show (codeNo c),
-    "/>\n"
+formatComment c =
+  concat
+    [ "<error "
+    , attr "line" $ show . lineNo $ c
+    , attr "column" $ show . colNo $ c
+    , attr "severity" . severity $ severityText c
+    , attr "message" $ messageText c
+    , attr "source" $ "ShellCheck.SC" ++ show (codeNo c)
+    , "/>\n"
     ]
 
-outputError file error = putStrLn $ concat [
-    "<file ", attr "name" file, ">\n",
-    "<error ",
-        attr "line" "1",
-        attr "column" "1",
-        attr "severity" "error",
-        attr "message" error,
-        attr "source" "ShellCheck",
-    "/>\n",
-    "</file>"
+outputError file error =
+  putStrLn $
+  concat
+    [ "<file "
+    , attr "name" file
+    , ">\n"
+    , "<error "
+    , attr "line" "1"
+    , attr "column" "1"
+    , attr "severity" "error"
+    , attr "message" error
+    , attr "source" "ShellCheck"
+    , "/>\n"
+    , "</file>"
     ]
 
+attr s v = concat [s, "='", escape v, "' "]
 
-attr s v = concat [ s, "='", escape v, "' " ]
 escape = concatMap escape'
-escape' c = if isOk c then [c] else "&#" ++ show (ord c) ++ ";"
+
+escape' c =
+  if isOk c
+    then [c]
+    else "&#" ++ show (ord c) ++ ";"
+
 isOk x = any ($x) [isAsciiUpper, isAsciiLower, isDigit, (`elem` " ./")]
 
 severity "error" = "error"
